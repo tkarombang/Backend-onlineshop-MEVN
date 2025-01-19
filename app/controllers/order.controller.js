865fdfc4e13d1d5446fe6a403dd956cc28a99aa1
tkarombang <tuangkarombang@gmail.com>
2025-01-20 00:57:38 +0800

    Menambahkan dan Menghapus Item product pada Orders Collection

--- app/controllers/order.controller.js
+++ app/controllers/order.controller.js
@@ -1,10 +1,11 @@
 const db = require('../models/database')
 const Order = db.orders
 
+//MENDAPATKAN SEMUA PRODUK
 exports.getAllOrder = async (req, res) => {
   const id = Number(req.params.id)
-
   try {
+    //MENGGUNAKAN MongoDB AGGREGATION
     const orders = await Order.aggregate([
       {
         $match: {
@@ -25,5 +26,72 @@ exports.getAllOrder = async (req, res) => {
     res.status(200).json(orders)
   } catch (error) {
     res.status(500).send({ message: error.message || 'SOME ERROR WHILE RETRIEVING ORDERS.' })
+  }
+}
+
+
+
+//MENAMBAHKAN ORDER
+exports.createOrder = async (req, res) => {
+  const id = Number(req.params.id)
+  const productCode = String(req.body.product)
+
+  try {
+    //CARI DOKUMEN BERDASARKAN user_id
+    const existingOrder = await Order.findOne({ user_id: id })
+    if (existingOrder && existingOrder.cart_items.includes(productCode)) {
+      return res.status(409).send({
+        message: "PRODUCT ALREADY EXIST"
+      })
+    }
+
+    const result = await Order.updateOne(
+      {
+        user_id: id //FILTER BERDASARKAN ID
+      },
+      {
+        $addToSet: { cart_items: productCode } // MENAMBAHKAN productCode ke cart_items
+      },
+      {
+        upsert: true // MEMBUAT DOKUMEN BARU JIKA TIDAK ADA user_id yang cocok
+      }
+    )
+
+    //KIRIM RESPON JIKA OPERASI BERHASIL
+    res.status(200).send({
+      message: 'ORDER UPDATE SUCCESFULLY',
+      result
+    })
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || 'AN  ERROR OCCURRED WHILE CREATING THE ORDER.'
+    })
+  }
+}
+
+
+exports.removeFromOrder = async (req, res) => {
+  const id = Number(req.params.id)
+  const productCode = String(req.params.product)
+
+  try {
+    const result = await Order.updateOne(
+      {
+        user_id: id //FILTER BERDASARKAN ID
+      },
+      {
+        $pull: { cart_items: productCode } // MENAMBAHKAN productCode ke cart_items
+      }
+    )
+
+    //KIRIM RESPON JIKA OPERASI BERHASIL
+    res.status(200).send({
+      message: 'ORDER DELETE SUCCESFULLY',
+      result
+    })
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || 'AN  ERROR OCCURRED WHILE REMOVE THE ORDER.'
+    })
   }
 }
--- app/routes/order.routes.js
+++ app/routes/order.routes.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const orderController = require('../controllers/order.controller')
 
 router.get('/user/:id', orderController.getAllOrder)
+router.post('/user/:id/add', orderController.createOrder)
+router.delete('/user/:id/product/:product', orderController.removeFromOrder)
 
 module.exports = router
