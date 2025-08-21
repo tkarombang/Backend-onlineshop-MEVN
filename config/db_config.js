18c301fec491cc68f902cd231ce2abc843a22f85
tkarombang <tuangkarombang@gmail.com>
2025-08-22 01:03:30 +0800

    deploy process

--- config/db_config.js
+++ config/db_config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
-  url: "mongodb://localhost:27017/collect_onlineshop"
+  url: process.env.MONGODB_URI || "mongodb://localhost:27017/collect_onlineshop"
 }
